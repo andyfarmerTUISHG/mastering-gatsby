2b32ad79488ac9ef617312785e225e1750b8cdeb
Andy Farmer <Andy.Farmer@travelopia.com>
2020-10-04 16:12:32 +0100

    MG13 - Data Relationships - Linked Toppings to Pizza Schema   - Added as type of array   - Created a specific preview     - Used specifc selects from toppings   - nifty use of filter(Boolean) to remove undefined

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -39,5 +39,37 @@ export default {
       validation: (Rule) => Rule.required().min(400).max(5000),
       // TODO: Add custom input component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'topping' }],
+        },
+      ],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    // Destructure the above and drop toppings into a new object
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter undefined from toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
